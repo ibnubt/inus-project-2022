ede55896a88600372a991dd6087e69901dd9bf03
ibnu.trisnandi <trisnandibagus@gmail.com>
2022-01-10 12:07:55 +0700

    edit quasar config

--- quasar.conf.js
+++ quasar.conf.js
@@ -49,7 +49,7 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/',
+      publicPath: '/airsale/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
