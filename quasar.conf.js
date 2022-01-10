c091d593a3f93c9f4b295ec066494d8b977faa75
ibnu.trisnandi <trisnandibagus@gmail.com>
2022-01-10 22:24:02 +0700

    test

--- quasar.conf.js
+++ quasar.conf.js
@@ -49,8 +49,8 @@ module.exports = configure(function (ctx) {
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
-      // publicPath: '/airsale/',
-      appBase: '/airsale/',
+      publicPath: '/airsale/',
+      // appBase: '/airsale/',
       // vueRouterBase: '/airsale/',
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
